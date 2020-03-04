206fc2168d310ac721e199ce51ebecf2ed230006
Nabeel Molham <n.molham@gmail.com>
2020-03-04 19:41:47 +0200

    Fix compatibility with NBE new system

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @author Nabeel Molham - n.molham@gmail.com
- * @version 1.0
+ * @version 2.0
  **/
+
+date_default_timezone_set( 'Africa/Cairo' );
 
 if ( '' === session_id() ) {
 	// start session to save prices
@@ -21,14 +23,35 @@ if ( '' === session_id() ) {
 // composer packages
 require_once __DIR__ . '/vendor/autoload.php';
 
-$init_amount = filter_input( INPUT_GET, 'amount', FILTER_SANITIZE_NUMBER_FLOAT );
-if ( ! $init_amount ) {
-	// init convert amount
-	$init_amount = 1;
+/**
+ * @return string
+ *
+ * @throws Exception
+ */
+function S4(): string {
+
+	$string = 'abcdef0123456789';
+
+	$s4 = $string[ random_int( 1, 15 ) ];
+	$s4 .= $string[ random_int( 1, 15 ) ];
+	$s4 .= $string[ random_int( 1, 15 ) ];
+	$s4 .= $string[ random_int( 1, 15 ) ];
+
+	return $s4;
+
 }
 
-$current_time = time();
-$page_data = nbe_load_html( 'nbe.temp', $current_time );
+/**
+ * @return string
+ *
+ * @throws Exception
+ */
+function generate_track_id(): string {
+
+	return S4() . S4() . '-' . S4() . '-4' . substr( S4(), 0, 3 ) .
+	       '-' . S4() . '-' . S4() . S4() . S4();
+
+}
 
 /**
  * @param string $file_name
@@ -36,97 +59,182 @@ $page_data = nbe_load_html( 'nbe.temp', $current_time );
  * @param int    $cache_duration
  *
  * @return array
+ * @throws Exception
  */
-function nbe_load_html( $file_name, $current_time, $cache_duration = 3600 ) {
+function nbe_load_html( $file_name, $current_time, $cache_duration = 3600 ): array {
+
+	/** @noinspection PhpUnhandledExceptionInspection */
+	$track_id  = generate_track_id();
+	$timestamp = date( 'Y-m-d\TH:i:s' );
 
 	if ( file_exists( $file_name ) && is_readable( $file_name ) ) {
 
-		$data = json_decode( file_get_contents( $file_name ), true );
+		/** @noinspection PhpComposerExtensionStubsInspection */
+		$cached_data = json_decode( file_get_contents( $file_name ), true );
 
-		if ( is_array( $data ) && $data['time'] + $cache_duration > $current_time ) {
+		if ( is_array( $cached_data ) && $cached_data['time'] + $cache_duration > $current_time ) {
 
-			return $data;
+			return $cached_data;
 
 		}
 
 	}
 
-	$data = [
-		'time' => time(),
-		'html' => file_get_contents( 'http://www.nbe.com.eg/en/ExchangeRate.aspx' ),
+	$input = [
+		'__type' => 'eChannelManagerBusinessXML.eBank',
+		'Item'   => [
+			'__type' => 'eChannelManagerBusinessXML.schExchangeRateGetRates',
+			'Body'   => [
+				'__type'                        =>
+					'eChannelManagerBusinessXML.schExchangeRateGetRatesBody',
+				'ExchangeRateGetRatesReqParams' => [
+					'__type'            =>
+						'eChannelManagerBusinessXML.schExchangeRateGetRatesBodyExchangeRateGetRatesReqParams',
+					'HighLightCurrency' => false,
+					'RequestType'       => 'ExchangeRates',
+				],
+			],
+			'Header' => [
+				'__type'                  => 'eChannelManagerBusinessXML.Header',
+				'Customer'                => [
+					'__type'               => 'eChannelManagerBusinessXML.CustomerHeaderInfoType',
+					'CustomerID'           => '',
+					'CustomerPin'          => '',
+					'CustDeviceID'         => '127.0.0.11',
+					'CustLoginIDOnChannel' => '_',
+				],
+				'FrontEnd'                => [
+					'FrontEndID'       => 'eCM-Web',
+					'FrontEndType'     => 'eCM-Web',
+					'FrontEndPassword' => '',
+				],
+				'Audit'                   => [
+					'__type'         => 'eChannelManagerBusinessXML.AuditHeaderInfoType',
+					'TransactionObj' => [
+						'__type'            => 'eChannelManagerBusinessXML.TransactionObjType',
+						'MasterLogLevel'    => '',
+						'TransactionID'     => $track_id,
+						'TransactionPath'   => 'ExchangeRateGetRates',
+						'TransactionType'   => 'ExchangeRateGetRates',
+						'TransCustID'       => $timestamp,
+						'TransFrontEndID'   => 'eCM-Mobile',
+						'TransFrontEndType' => 'eCM-Web',
+					],
+					'SessionObj'     => [
+						'__type'       => 'eChannelManagerBusinessXML.SessionObjType',
+						'SessionObjID' => 'Session_',
+					],
+				],
+				'User'                    => [
+					'__type'  => 'eChannelManagerBusinessXML.UserHeaderInfoType',
+					'UserID'  => '',
+					'UserPin' => '',
+				],
+				'MemoList'                => [
+					'MemoItem1' => '',
+					'MemoItem6' => '',
+					'MemoItem7' => '',
+					'MemoItem8' => '',
+					'MemoItem2' => 'false',
+				],
+				'Service'                 => [
+					'__type'                      => 'eChannelManagerBusinessXML.ServiceHeaderInfoType',
+					'ServiceID'                   => 'ExchangeRateGetRates',
+					'ServiceMessageType'          => 'ExchangeRateGetRates',
+					'ServiceRequestID'            => $track_id,
+					'ServiceRequestLanguageCode'  => 'EN',
+					'ServiceRequestTime'          => $timestamp . '.000Z',
+					'ServiceRequestTimeSpecified' => true,
+					'ServiceResult'               => [
+						'__type'     =>
+							'eChannelManagerBusinessXML.ResultHeaderInfoType',
+						'ResultCode' => '0',
+						'ResultDesc' => '',
+					],
+				],
+				'CachingAndExpiryControl' => [
+					'__type'            =>
+						'eChannelManagerBusinessXML.HeaderCachingAndExpiryControl',
+					'DataHashSignature' => '',
+				],
+			],
+		],
 	];
 
-	if ( empty( $data['html'] ) ) {
+	/** @noinspection PhpComposerExtensionStubsInspection */
+	$response = json_decode( file_get_contents(
+		'https://www.nbe.com.eg/NBEeChannelManager/CallMW.aspx?TrackID=' . $track_id,
+		false,
+		stream_context_create( [
+			'http' => [
+				'method'  => 'POST',
+				'header'  =>
+					'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+				'content' => http_build_query( [
+					'I_InputObjectJSONStr' => json_encode( $input ),
+				] ),
+			],
+		] )
+	), true );
+
+	$data = [
+		'time' => time(),
+		'list' => @$response['RspObj']['Item']['Body']['ExchangeRateGetRatesRspParams']['Currencies'],
+	];
+
+	if ( empty( $data['list'] ) ) {
+
+		if ( isset( $cached_data ) && is_array( $cached_data ) ) {
+
+			return $cached_data;
+
+		}
 
 		// display error if network error happened
 		die( '<h1>Unable ot load data from NBE website!</h1>' );
 
 	}
 
+	/** @noinspection PhpComposerExtensionStubsInspection */
 	file_put_contents( $file_name, json_encode( $data ) );
 
 	return $data;
+
 }
 
-// init phpQuery
-$dom = pQuery::parseStr( $page_data['html'] );
+$init_amount = filter_input( INPUT_GET, 'amount', FILTER_SANITIZE_NUMBER_FLOAT ) ?? 1;
 
-$prices = [
-	'usd' => [
-		'title'    => 'US DOLLAR',
-		'code'     => '#dgPrices tr:nth-child(2) td:nth-child(2)',
-		'selector' => [
-			'buy'           => '#dgPrices tr:nth-child(2) td:nth-child(3)',
-			'sell'          => '#dgPrices tr:nth-child(2) td:nth-child(4)',
-			'buy_transfer'  => '#dgPrices tr:nth-child(2) td:nth-child(5)',
-			'sell_transfer' => '#dgPrices tr:nth-child(2) td:nth-child(6)',
-		],
-	], // USD selectors
-	'eur' => [
-		'title'    => 'EURO',
-		'code'     => '#dgPrices tr:nth-child(3) td:nth-child(2)',
-		'selector' => [
-			'buy'           => '#dgPrices tr:nth-child(3) td:nth-child(3)',
-			'sell'          => '#dgPrices tr:nth-child(3) td:nth-child(4)',
-			'buy_transfer'  => '#dgPrices tr:nth-child(3) td:nth-child(5)',
-			'sell_transfer' => '#dgPrices tr:nth-child(3) td:nth-child(6)',
-		],
-	], // EUR selectors
-	'aud' => [
-		'title'    => 'AUSTRALIAN DOLLAR',
-		'code'     => '#dgPrices tr:nth-child(11) td:nth-child(2)',
-		'selector' => [
-			'buy'           => '#dgPrices tr:nth-child(11) td:nth-child(3)',
-			'sell'          => '#dgPrices tr:nth-child(11) td:nth-child(4)',
-			'buy_transfer'  => '#dgPrices tr:nth-child(11) td:nth-child(5)',
-			'sell_transfer' => '#dgPrices tr:nth-child(11) td:nth-child(6)',
-		],
-	], // AUD selectors
-];
+$current_time = time();
 
-// selectors holder
-$els = null;
+/** @noinspection PhpUnhandledExceptionInspection */
+$prices_data = nbe_load_html( 'nbe.temp', $current_time );
+
+$prices = [];
+
+$wanted_currencies = [ 'USD', 'EURO', 'GBP', 'AUD', 'CAD', 'SAR', 'AED' ];
 
 // prices values loop
-foreach ( $prices as $currency_code => $args ) {
+foreach ( $prices_data['list'] as $currency ) {
 
-	// unit code
-	$prices[ $currency_code ]['code'] = trim( $dom->query( $args['code'] )->text() );
+	$currency_code = $currency['CurrencyISOCode'];
 
-	// rates
-	foreach ( $args['selector'] as $selector_name => $selector_query ) {
+	if ( false === in_array( $currency_code, $wanted_currencies, true ) ) {
 
-		// query selectors
-		$selector_val = trim( $dom->query( $selector_query )->text() );
-
-		$prices[ $currency_code ][ $selector_name ] = empty( $selector_val ) ? 0 : (float) $selector_val;
+		continue;
 
 	}
+
+	$prices[ $currency_code ]['code']          = $currency_code;
+	$prices[ $currency_code ]['title']         = $currency['Name_AR'] . ' - ' . $currency['Name_EN'];
+	$prices[ $currency_code ]['buy']           = $currency['CashBuyRate'] ?? 0;
+	$prices[ $currency_code ]['sell']          = $currency['CashSellRate'] ?? 0;
+	$prices[ $currency_code ]['buy_transfer']  = $currency['TransferBuyRate'] ?? 0;
+	$prices[ $currency_code ]['sell_transfer'] = $currency['TransferSellRate'] ?? 0;
 
 }
 
 ?><!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -146,17 +254,18 @@ foreach ( $prices as $currency_code => $args ) {
 <?php
 // prices layouts loop
 foreach ( $prices as $currency_code => $args ) {
-	echo '<h2 class="currency-title" data-cur="', $currency_code, '">', $args['title'], ': <span></span></h2>';
+
+	echo '<h2 class="currency-title" data-cur="', $currency_code, '">', $args['title'], ' : <span></span></h2>';
 	echo '<div class="results">';
 	echo '<label>Buy: <input type="text" readonly data-cur="', $currency_code, '" data-method="buy" /></label>';
 	echo '<label>Sell: <input type="text" readonly data-cur="', $currency_code, '" data-method="sell" /></label>';
 	echo '<label>Transfers/Buy: <input type="text" readonly data-cur="', $currency_code, '" data-method="buy_transfer" /></label>';
 	echo '<label>Transfers/Sell: <input type="text" readonly data-cur="', $currency_code, '" data-method="sell_transfer" /></label></div>';
+
 }
 ?>
-
 <!-- footer -->
-<p class="note">Note: Info updated every 5 minutes</p>
+<p class="note">Note: Info updated every 1 hour</p>
 <a href="https://github.com/N-Molham/currency_exchange" class="fork">
 	<img src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub">
 </a>
--- script.js
+++ script.js
@@ -1,61 +1,84 @@
 (function ( env, $ ) {
+
 	// jQuery document ready
 	$( function () {
+
 		// currencies' titles
 		var $titles = $( '.currency-title' );
 
 		// results fields objects
 		var $fields = $( '.results input' ).on( 'nbe.calcResult', function ( e, amount ) {
-			var $this         = $( this ),
-			    result_amount = 0,
-			    element       = e.currentTarget;
+
+			var $this   = $( this ), result_amount,
+			    element = e.currentTarget;
 
 			if ( element.dataset ) {
+
 				// JS modern browsers way
 				result_amount = amount * prices[ element.dataset.cur ][ element.dataset.method ];
+
 			} else if ( $this.data ) {
+
 				// jQuery modern way
 				result_amount = amount * prices[ $this.data( 'cur' ) ][ $this.data( 'method' ) ];
+
 			} else {
+
 				// jQuery old way
 				result_amount = amount * prices[ $this.attr( 'data-cur' ) ][ $this.attr( 'data-method' ) ];
+
 			}
 
 			// display rounded value
 			$this.val( round_number( result_amount ) + ' EGP' );
+
 		} );
 
 		// listen for keyup and change events on amount field
 		$( '#amount' ).on( 'keyup change nbe-change', function ( e ) {
+
 			// the amount
 			var amount = parseFloat( e.target.value );
 
 			// if not a number make it 1
 			if ( isNaN( amount ) ) {
+
 				amount = 1;
+
 			}
 
 			// update title
 			$titles.each( function () {
+
 				var $this = $( this );
 
 				$this.find( 'span' ).html( amount + ' ' + prices[ $this.data( 'cur' ) ].code );
+
 			} );
 
 			// update result
-			$fields.trigger( 'nbe.calcResult', [ amount ] );
+			$fields.trigger( 'nbe.calcResult', [amount] );
+
 		} ).trigger( 'nbe-change' ); // trigger change event on page load
+
 	} );
 
 	// round result number to 4 decimals
 	env.round_number = function ( num ) {
+
 		return Math.round( num * 10000 ) / 10000;
+
 	};
 
 	// console log if found
 	env.trace = function ( any ) {
+
 		if ( env.console && console.log ) {
+
 			console.log( any );
+
 		}
+
 	};
+
 })( window, jQuery ); // self-executable anonymous function
